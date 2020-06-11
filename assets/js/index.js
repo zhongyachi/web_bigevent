d433ad38ec9d36b190d6cef5437521eb364317e1
zyc <zhongyachi@126.com>
2020-06-12 00:45:09 +0800

    完成个人中心功能的开发

--- assets/js/index.js
+++ assets/js/index.js
@@ -70,7 +70,7 @@ function renderAvatar(user) {
         // 3.按需渲染用户的头像
     if (user.user_pic !== null) {
         // 3.1渲染图片头像
-        $('.layui-nav-img').attr('src', user, user_pic).show()
+        $('.layui-nav-img').attr('src', user.user_pic).show()
         $('.text-avatar').hide()
 
     } else {
---
+++ assets/js/user/user_info.js
@@ -0,0 +1,78 @@
+$(function() {
+    var form = layui.form
+    var layer = layui.layer
+
+
+    form.verify({
+        nickname: function(value) {
+            if (value.length > 6) {
+                return '昵称长度必须在1~6个字符之间'
+            }
+        }
+    })
+
+    inituserinfo()
+
+    //    初始化用户的基本信息
+    function inituserinfo() {
+
+        $.ajax({
+
+            method: 'GET',
+            url: '/my/userinfo',
+            success: function(res) {
+
+                if (res.status !== 0) {
+
+                    return layer.msg('获取用户信息成功')
+                }
+
+                console.log(res)
+                    // 调用form.val()快速为表单赋值
+                form.val('formUserinfo', res.data)
+            }
+
+
+        })
+
+
+    }
+
+    // 重置表单对象
+
+    $('#btnReset').on('click', function(e) {
+        //    阻止表单默认事件
+        e.preventDefault()
+
+        inituserinfo()
+
+    })
+
+
+    // 监听表单提交事件
+    $('.layui-form').on('submit', function(e) {
+
+        //    阻止表单默认事件
+        e.preventDefault()
+            // 发起ajax请求
+        $.ajax({
+            method: 'POST',
+            url: '/my/userinfo',
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('更新用户信息失败！')
+                }
+
+                layer.msg('更新用户信息成功！')
+                    // 调用父页中的方法,重新渲染用户的头像和用户的信息
+                window.parent.getUserInfo()
+            }
+
+        })
+
+
+    })
+
+
+})
